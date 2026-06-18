ed0a2350909397e93888cf1ed420344e47dfeb64
deepika bandhanadham <deepika.bandhanadham+1781632592@dbtlabsworkshop.com>
2026-06-18 18:58:32 +0000

    Implemented source freshness and created models yml in individual folders

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,9 @@
+-- cte are just a convention and can be defined as a sql
+with
+    customers as (
+
+        select id as customer_id, first_name, last_name from {{ source('jaffle_shop', 'customers') }}
+    )
+
+select *
+from customers 
