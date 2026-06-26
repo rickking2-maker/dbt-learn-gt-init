5ecec29313b63d446171dacbdd80451aafd7cd66
rickking2-maker <rick.king2@gmail.com>
2026-06-26 17:05:55 -0400

    updated based on cents and successful transaction

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -8,15 +8,23 @@ orders as (
 payments as (
     select * from {{ref('stg_stripe_payments')}}
 ),
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount      
+    from payments
+    group by 1
+),
+
 final as (
     select
         orders.order_id,
         orders.customer_id,
-        payments.amount
-
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
     from orders
 
-    left join payments using (order_id)
+    left join order_payments using (order_id)
 
 )
 select * from final
--- models/staging/stg_stripe_payments.sql
+++ models/staging/stg_stripe_payments.sql
@@ -1,7 +1,11 @@
 
 select
+    id as payment_id,
     orderid as order_id,
-    amount
+    paymentmethod as payment_method,
+    status,
+    --amount is stored in cents, so we need to divide by 100 to get dollars
+    amount /100 as amount
 
 from raw.stripe.payment
 
